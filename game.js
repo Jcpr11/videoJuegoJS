8e547369bfdab533d9e79f6ae69938ff0339a1a7
Jcpr11 <camieros21@gmail.com>
2022-10-23 12:23:22 -0500

    Se separan funciones en js

--- game.js
+++ game.js
@@ -1,10 +1,34 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener("load", startGame);
+let canvasSize;
+let elementsSize;
+
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
+
+function setCanvasSize() {
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute("width", canvasSize);
+    canvas.setAttribute("height", canvasSize);
+
+    elementsSize = canvasSize / 10;
+
+    startGame();
+}
 
 function startGame() {
-    //game.fillRect(0,0,100,100);
-    //game.clearRect(50,50,50,50);
-    game.fillText("Hola", 20, 20);
+    console.log({canvasSize, elementsSize});
+
+    game.font = elementsSize + "px Verdana";
+    game.textAlign = "end";
+    
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis['X'], elementsSize  * i, elementsSize);
+    }
 }
