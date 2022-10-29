8987c1ec580cf1868e392042c308bb3b0c801419
Jcpr11 <camieros21@gmail.com>
2022-10-28 20:26:55 -0500

    Se completan las renderizaciones de la calaca cada que se mueve y se impide que salga del cuadro

--- game.js
+++ game.js
@@ -4,8 +4,20 @@ const game = canvas.getContext('2d');
 let canvasSize;
 let elementsSize;
 
+/* variable de jugador */
+
+const playerPosition = {
+    x: undefined,
+    y: undefined,
+}
+
+
+/* add event listener canvas y resize pagina */
+
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
+
+/* Funciones inciales size etc */
 
 function setCanvasSize() {
     if (window.innerHeight > window.innerWidth) {
@@ -34,12 +46,112 @@ function startGame() {
     const mapRowCols = mapRows.map(row => row.trim().split(""));
     console.log(mapRowCols);
 
+    game.clearRect(0, 0, canvasSize, canvasSize);
+
     mapRowCols.forEach((row, rowI) => {
         row.forEach((col, colI) => {
             const emoji = emojis[col];
             const posX = elementsSize * (colI + 1);
             const posY = elementsSize * (rowI + 1);
+
+            if (col == 'O') {
+                if (!playerPosition.x && !playerPosition.y) {
+                    playerPosition.x = posX;
+                    playerPosition.y = posY;
+                    console.log({playerPosition});
+                }
+            }
+
             game.fillText(emoji, posX, posY);
         })
     });
+
+    movePlayer();
+}
+
+/* Variables de los botones */
+
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
+
+/* Add event listener de las teclas */
+
+window.addEventListener("keydown", moveByKey);
+
+/* add event listener de los botones */
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
+
+/* funciones de movimiento */
+
+function moveByKey(event) {
+    console.log(event);
+    switch (event.key) {
+        case 'ArrowUp':
+            moveUp();
+            break;
+        case 'ArrowLeft':
+            moveLeft();
+            break;
+        case 'ArrowRight':
+            moveRight();
+            break;
+        case 'ArrowDown':
+            moveDown();
+            break;
+    }
+}
+
+/* renderizado y movimiento de la calaca */
+
+function movePlayer() {
+    game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+function moveUp() {
+    console.log("Me quiero mover hacia arriba");
+
+    if ((playerPosition.y - elementsSize) < elementsSize) {
+        console.log("Out");
+    } else {
+        playerPosition.y -= elementsSize;
+        startGame();
+    }    
+}
+
+function moveLeft() {
+    console.log("Me quiero mover hacia la izquierda");
+
+    if ((playerPosition.x - elementsSize) < elementsSize) {
+        console.log("Out");
+    } else {
+        playerPosition.x -= elementsSize;
+        startGame();
+    }
+}
+
+function moveRight() {
+    console.log("Me quiero mover hacia arriba la derecha");
+
+    if ((playerPosition.x + elementsSize) > canvasSize) {
+        console.log("Out");
+    } else {
+        playerPosition.x += elementsSize;
+        startGame();
+    }
+}
+
+function moveDown() {
+    console.log("Me quiero mover hacia abajo");
+
+    if ((playerPosition.y + elementsSize) > canvasSize) {
+        console.log("Out");
+    } else {
+        playerPosition.y += elementsSize;
+        startGame();
+    }
 }
