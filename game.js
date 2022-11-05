1f5f576fc5d61ce6fdb2ee796e394b22ca777a11
Jcpr11 <camieros21@gmail.com>
2022-11-05 16:40:55 -0500

    Se debugea el juego y se ajistan estilos ver 1.5

--- game.js
+++ game.js
@@ -59,21 +59,29 @@ btnDown.addEventListener("click", moveDown);
 /* Funciones inciales size etc */
 function setCanvasSize() {
     if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.8;
+        canvasSize = window.innerWidth * 0.7;
     } else {
-        canvasSize = window.innerHeight * 0.8;
+        canvasSize = window.innerHeight * 0.7;
     }
+
+    canvasSize = Number(canvasSize.toFixed(0));
 
     canvas.setAttribute("width", canvasSize);
     canvas.setAttribute("height", canvasSize);
 
     elementsSize = canvasSize / 10;
+    
+    elementsSize = Number(elementsSize.toFixed(0));
+
+    playerPosition.x = undefined;
+    playerPosition.y = undefined;
 
     startGame();
 }
 
 function startGame() {
     console.log({canvasSize, elementsSize});
+    console.log(playerPosition, giftPosition);
 
     game.font = elementsSize + "px sans-serif";
     game.textAlign = "end";
