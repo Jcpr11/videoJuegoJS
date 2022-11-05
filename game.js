95269cccaa8544ddd0f19b56ed327249d9f44fb7
Jcpr11 <camieros21@gmail.com>
2022-11-04 20:20:52 -0500

    Se terminan las logicas del juego y se despliega la version 1.0 del juego, se corrigen algunos estilos

--- game.js
+++ game.js
@@ -1,24 +1,62 @@
+/* Selectores Body y Canvas */
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
+/* Selectores de los botones */
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
+
+/* Selectores vidas y tiempo */
+const spanLives = document.querySelector('#lives');
+const spanTime = document.querySelector('#time');
+
+/* Selectores records y tiempo partida total */
+const spanRecord = document.querySelector('#record');
+const pResult = document.querySelector('#result');
+
+/* Variables globales */
 let canvasSize;
 let elementsSize;
+let level = 0;
+let lives = 3;
+let timeStart;
+let timePlayer;
+let timeInterval;
+
 
 /* variable de jugador */
 
 const playerPosition = {
     x: undefined,
     y: undefined,
-}
+};
 
+/* Colisiones */
+
+const giftPosition = {
+    x: undefined,
+    y: undefined,
+};
+
+let enemyPositions = [];
 
 /* add event listener canvas y resize pagina */
-
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
 
-/* Funciones inciales size etc */
+/* Add event listener de las teclas */
+window.addEventListener("keydown", moveByKey);
 
+/* add event listener de los botones */
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
+
+
+/* Funciones inciales size etc */
 function setCanvasSize() {
     if (window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.8;
@@ -40,12 +78,27 @@ function startGame() {
     game.font = elementsSize + "px sans-serif";
     game.textAlign = "end";
 
-    const map = maps[0];
+    const map = maps[level];
+
+    if (!map) {
+        gameWin();
+        return;
+    }
+
+    if (!timeStart) {
+        timeStart = Date.now();
+        timeInterval = setInterval(showTime, 100);
+        showRecord();
+    }
+
     const mapRows = map.trim().split("\n");
     console.log(mapRows);
     const mapRowCols = mapRows.map(row => row.trim().split(""));
     console.log(mapRowCols);
 
+    showLives();
+
+    enemyPositions = [];
     game.clearRect(0, 0, canvasSize, canvasSize);
 
     mapRowCols.forEach((row, rowI) => {
@@ -60,6 +113,14 @@ function startGame() {
                     playerPosition.y = posY;
                     console.log({playerPosition});
                 }
+            } else if (col == 'I') {
+                giftPosition.x = posX;
+                giftPosition.y = posY;
+            } else if (col == 'X') {
+                enemyPositions.push({
+                    x: posX,
+                    y: posY
+                });
             }
 
             game.fillText(emoji, posX, posY);
@@ -68,23 +129,6 @@ function startGame() {
 
     movePlayer();
 }
-
-/* Variables de los botones */
-
-const btnUp = document.querySelector("#up");
-const btnLeft = document.querySelector("#left");
-const btnRight = document.querySelector("#right");
-const btnDown = document.querySelector("#down");
-
-/* Add event listener de las teclas */
-
-window.addEventListener("keydown", moveByKey);
-
-/* add event listener de los botones */
-btnUp.addEventListener("click", moveUp);
-btnLeft.addEventListener("click", moveLeft);
-btnRight.addEventListener("click", moveRight);
-btnDown.addEventListener("click", moveDown);
 
 /* funciones de movimiento */
 
@@ -106,11 +150,106 @@ function moveByKey(event) {
     }
 }
 
-/* renderizado y movimiento de la calaca */
+/* renderizado y movimiento y choques de la calaca */
 
 function movePlayer() {
+
+    /* Comprobacion si se colisiona con el regalo */
+
+    const giftCollisionX = playerPosition.x.toFixed(2) == giftPosition.x.toFixed(2);
+    const giftCollisionY = playerPosition.y.toFixed(2) == giftPosition.y.toFixed(2);
+    const giftCollision = giftCollisionX && giftCollisionY;
+
+    if (giftCollision) {
+        levelWin();
+    }
+
+    /* comprobacion si se choca con una bomba */
+
+    const enemyCollision = enemyPositions.find(enemy => {
+        const enemyCollisionX = enemy.x.toFixed(2) == playerPosition.x.toFixed(2);
+        const enemyCollisionY = enemy.y.toFixed(2) == playerPosition.y.toFixed(2);
+        return enemyCollisionX && enemyCollisionY;
+    });
+
+    if (enemyCollision) {
+        levelFail();
+    }
+
     game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
 }
+
+/* Juego ganado o nivel ganado o perdido*/
+
+function levelWin() {
+    console.log('Subiste de nivel');
+    level++;
+    startGame();
+}
+
+function gameWin() { // ejecuta la finalizacion del juego y pone los records
+    console.log('Terminaste el juego !');
+    clearInterval(timeInterval);
+
+    const recordTime = localStorage.getItem('record_time');
+    const playerTime = Date.now() - timeStart;
+
+    if (recordTime) {
+        if (recordTime >= playerTime) {
+            localStorage.setItem('record_time', playerTime);
+            pResult.innerHTML = 'Superaste el record! 🥳';
+        } else {
+            pResult.innerHTML = 'Lo siento no superaste el record 😒';
+        }
+    } else {
+        localStorage.setItem('record_time', playerTime);
+        pResult.innerHTML = 'Has logrado un nuevo record! 🤩';
+    }
+
+    console.log({recordTime, playerTime});
+}
+
+function levelFail() {
+    console.log('Perdiste una vida');
+
+    lives--;
+
+    console.log(lives);
+
+    if (lives <= 0) {
+        level = 0;
+        lives = 3;
+        timeStart = undefined;
+    }
+
+    playerPosition.x = undefined;
+    playerPosition.y = undefined;
+    startGame();
+}
+
+/* funciones para incluir los corazones y el tiempo y el record y el tiempo */
+
+function showLives() {
+    const heartsArray = Array(lives).fill(emojis['HEART']) // crea un array
+    console.log(heartsArray);
+    let heartsText = heartsArray.toString(); // pasamos el array a string
+    console.log(heartsText);
+    let heartsNoCommas = heartsText.replace(/,/g, ''); // quitamos todas las comas
+    console.log(heartsNoCommas);
+
+    spanLives.innerHTML = heartsNoCommas;
+}
+
+function showTime() {
+    spanTime.innerHTML = Date.now() - timeStart;
+}
+
+function showRecord() {
+    spanRecord.innerHTML = localStorage.getItem('record_time');
+}
+
+
+/* funciones de movimiento */
 
 function moveUp() {
     console.log("Me quiero mover hacia arriba");
